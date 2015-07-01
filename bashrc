c4e9f893be1c955d9f15358ac6bb83ece8934121
root <root@ubuntu.(none)>
2015-07-01 12:14:52 +0800

    update config

--- bashrc
+++ bashrc
@@ -81,7 +81,7 @@ esac
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    alias ls='ls -AF --color=auto'
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
 
@@ -92,7 +92,6 @@ fi
 
 # some more ls aliases
 alias ll='ls -AlFh'
-alias la='ls -A'
 alias l='ls -CF'
 alias h='history | tail -n 25'
 alias cp='cp -v'
--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-#create symlinks from home directory to ~/dotfiles
 
 
 dir=~/dotfiles
@@ -9,6 +8,7 @@ files="vimrc bashrc gitconfig vim"
 
 mkdir -p $backup
 
+#create symlinks from home directory to ~/dotfiles
 for file in $files; do
   echo "Backup $file"
   mv ~/.$file $backup
@@ -16,8 +16,18 @@ for file in $files; do
   ln -s $dir/$file ~/.$file
 done
 
+#config apt
+#echo "config apt"
+#mv /etc/apt/sources.list /etc/apt/sources.list.bak
+#ln -s $dir/etc/sources.list /etc/apt/sources.list
+#apt-get update
+
 #install cscope
 
 #install ctags
 
 #install vim7.4+
+
+#install svn
+#apt-get install subversion
+

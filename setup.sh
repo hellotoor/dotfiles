91c65a481f06e77890f2ec23792b4cfd937277d9
dancing4you <dancing4you@163.com>
2015-08-22 06:58:34 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,65 @@
 #! /bin/sh
+cecho()
+{
+  while [ $# -gt 0 ]
+  do
+    case $1 in
+      -b)
+        /bin/echo -ne " ";
+        ;;
+      -t)
+        /bin/echo -ne "\t";
+        ;;
+      -n)     /bin/echo -ne "\n";
+        ;;
+      -black)
+        /bin/echo -ne "\033[30m";
+        ;;
+      -red)
+        /bin/echo -ne "\033[31m";
+        ;;
+      -green)
+        /bin/echo -ne "\033[32m";
+        ;;
+      -yellow)
+        /bin/echo -ne "\033[33m";
+        ;;
+      -blue)
+        /bin/echo -ne "\033[34m";
+        ;;
+      -purple)
+        /bin/echo -ne "\033[35m";
+        ;;
+      -cyan)
+        /bin/echo -ne "\033[36m";
+        ;;
+      -white|-gray)
+        /bin/echo -ne "\033[37m";
+        ;;
+      -reset)
+        /bin/echo -ne "\033[0m";
+        ;;
+      *)
+        /bin/echo -n "$1"
+        ;;
+    esac
+    shift
+  done
+  /bin/echo -e "\033[0m"
+}
 
 usage_and_exit()
 {
-  echo "Config dotfiles and common tools!"
-  echo "Usage: ./setup [OPTION]"
-  echo ""
-  echo "    -h print this help and exit"
-  echo "    -q quick config mode"
-  echo "    -a config all"
-  echo ""
+  /bin/echo "Config dotfiles and common tools!"
+  /bin/echo "Usage: ./setup [OPTION]"
+  /bin/echo ""
+  /bin/echo "    -h print this help and exit"
+  /bin/echo "    -q quick config mode"
+  /bin/echo "    -a config all"
+  /bin/echo ""
 
   exit 0
 }
-
 
 mode="default"
 
@@ -28,14 +75,11 @@ for arg in $@; do
       mode="all"
       ;;
     *)
-      echo "Invalid option!"
+      /bin/echo "Invalid option!"
       usage_and_exit
       ;;
   esac
 done
-
-echo "run in $mode mode!"
-exit 0
 
 dotdir=~/dotfiles
 backup=~/dotfiles_backup
@@ -44,69 +88,105 @@ files="tmux.conf vimrc bashrc gitconfig vim"
 mkdir -p $backup
 
 #create symlinks from home directory to ~/dotfiles
+/bin/echo ""
+cecho -yellow "*********************config dotfiles************************"
+/bin/echo ""
 for file in $files; do
-  echo "Backup $file"
-  mv ~/.$file $backup 2 > /dev/null
-  echo "create symlink to $dotdir/$file"
+  /bin/echo "backup $file"
+  mv ~/.$file $backup 2> /dev/null
+  /bin/echo "create symlink from ~/.$file to $dotdir/$file"
   ln -s $dotdir/$file ~/.$file
 done
 
-#remove vim info, vim will create it automaticly
+#remove vim info in case permission issue, vim will create it automaticly
 rm -f ~/.viminfo
 
 #config apt
-echo "Config apt"
-echo "Which sources.list do you want to use?"
-echo "1 file in git server(Suggest)"
-echo "2 auto detect use apt-spy(Slowly)"
-echo "3 not change"
+/bin/echo ""
+cecho -yellow "*********************config apt tool************************"
+/bin/echo ""
 
-read choice
-case "$choice" in 
-  "1")
-    echo "Backup old file to /etc/apt/sources.list.bak"
-    mv /etc/apt/sources.list /etc/apt/sources.list.bak
-    ln -s $dotdir/etc/apt-spy.list /etc/apt/sources.list
-    ;;
-  "2")
-    ;;
-  *)
-    echo "sources.list not changed"
-    ;;
-esac
+if [ "$mode" = "default" ]; then
+  /bin/echo "Which sources.list do you want to use?"
+  /bin/echo "1 file in git server(Suggest)"
+  /bin/echo "2 auto detect use apt-spy(Slowly)"
+  /bin/echo "3 not change"
 
-echo "Update apt source?[y/n]"
-read choice
-case "$choice" in 
-  y|Y)
-    apt-get update
-    ;;
-  *)
-    ;;
-esac 
+  read choice
+  case "$choice" in 
+    "1")
+      /bin/echo "Backup old file to /etc/apt/sources.list.bak"
+      mv /etc/apt/sources.list /etc/apt/sources.list.bak
+      ln -s $dotdir/etc/apt-spy.list /etc/apt/sources.list
+      ;;
+    "2")
+      $dotdir/bin/apt-spy update
+      ln -sf $dotdir/etc/apt-spy.conf /etc/apt-spy.conf 
+      $dotdir/bin/apt-spy -a Asia -d stable
+      ;;
+    *)
+      /bin/echo "sources.list not changed"
+      ;;
+  esac
+elif [ "$mode" = "quick" ]; then
+  /bin/echo "Backup old file to /etc/apt/sources.list.bak"
+  mv /etc/apt/sources.list /etc/apt/sources.list.bak
+  ln -s $dotdir/etc/apt-spy.list /etc/apt/sources.list
+elif [ "$mode" = "all"]; then
+  $dotdir/bin/apt-spy update
+  ln -sf $dotdir/etc/apt-spy.conf /etc/apt-spy.conf 
+  $dotdir/bin/apt-spy -a Asia -d stable
+fi
 
+if [ "$mode" = "default" ]; then
+  /bin/echo "Update apt source?[y/n]"
+  read choice
+  case "$choice" in 
+    y|Y)
+      apt-get update
+      ;;
+    *)
+      ;;
+  esac 
+else 
+  apt-get update
+fi
+
+
+#install common tools
+/bin/echo ""
+cecho -yellow "*********************config common tools********************"
+/bin/echo ""
 result=`which cscope`
 if [ -z "$result" ]; then
-  echo "install cscope"
+  /bin/echo "install cscope"
   apt-get install cscope
+else 
+  /bin/echo "cscope is already exist."
 fi
 
 result=`which ctags`
 if [ -z "$result" ]; then
-  echo "install ctags"
+  /bin/echo "install ctags"
   apt-get install exuberant-ctags 
+else 
+  /bin/echo "ctags is already exist."
 fi
 
 result=`which svn`
 if [ -z "$result" ]; then
-  echo "install svn"
+  /bin/echo "install svn"
   apt-get install subversion 
+else 
+  /bin/echo "svn is already exist."
 fi
 
 result=`which tmux`
 if [ -z "$result" ]; then
-  echo "install tmux"
+  /bin/echo "install tmux"
   apt-get install tmux 
+else 
+  /bin/echo "tmux is already exist."
 fi
 
 

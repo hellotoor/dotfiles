5e4c9a69b46527a84396c6b6d322b61bbe6d5005
dancing4you <dancing4you@163.com>
2017-10-26 11:29:07 +0800

    update

--- code_sample/util.c
+++ code_sample/util.c
@@ -15,3 +15,27 @@ void hexdump(char *buf, int buf_len)
     }
     printf("\n");
 }
+
+char *memmem(char *src, int src_len, char *sub, int sub_len)
+{
+	int i, j;
+
+	if(src == NULL || sub == NULL || src_len <= 0|| sub_len <= 0 || src_len < sub_len)
+		return NULL;
+
+	for (i = 0; i < src_len - sub_len; i++) 
+	{
+		for (j = 0; j < sub_len; j++) 
+		{
+			if(src[i+j] != sub[j])
+				break;
+		}
+
+		if(j >= sub_len) 
+		{
+			return src + i;
+		}
+	}
+
+	return NULL;
+}

eadf9ca2da8c2da657c48ba50543feaf359ff74f
dancing4you <dancing4you@163.com>
2017-11-22 03:32:00 -0500

    update

--- bin/l2tp_install.sh
+++ bin/l2tp_install.sh
@@ -1,11 +1,19 @@
 apt-get install xl2tpd
 apt-get install strongswan
-cp -f ~/dotfiles/etc/xl2tpd.conf /etc/xl2tpd/
-cp -f ~/dotfiles/etc/chap-secrets /etc/ppp/
-cp -f ~/dotfiles/etc/options.xl2tpd /etc/ppp/
-cp -f ~/dotfiles/etc/ /etc/ppp/
 
-ip = $(ip route get 8.8.8.8 | grep -v cache | cut -d ' ' -f 7)
+mv /etc/xl2tpd/xl2tpd.conf /etc/xl2tpd/xl2tpd.conf.bak 2> /dev/null
+cp -f ~/dotfiles/etc/xl2tpd.conf /etc/xl2tpd/xl2tpd.conf
+
+mv /etc/ppp/chap-secrets /etc/ppp/chap-secrets.bak 2> /dev/null
+cp -f ~/dotfiles/etc/chap-secrets /etc/ppp/
+
+cp -f ~/dotfiles/etc/options.xl2tpd /etc/ppp/
+
+mv /etc/ipsec.conf  /etc/ipsec.conf.bak
+cp -f ~/dotfiles/etc/ipsec.conf /etc/ipsec.conf
+
+#ip = $(ip route get 8.8.8.8 | grep -v cache | cut -d ' ' -f 7)
+ip=$(ip route get 8.8.8.8 | awk -F"src" '/src/{gsub(/ /,"");print $2}')
 echo '$ip : PSK \"lock123456\"' >> /etc/ipsec.secrets
 
 

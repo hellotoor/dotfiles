8e050523497df8a791f8e804e0d7dba681e01854
dancing4you <dancing4you@163.com>
2018-11-12 08:07:17 +0000

    update vpn install

--- install/openvpn/install.sh
+++ install/openvpn/install.sh
@@ -6,4 +6,6 @@ cp -fr ./files/* /etc/openvpn/
 
 openvpn --daemon --config /etc/openvpn/server.conf
 
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j MASQUERADE
+
 echo "Done."
--- install/shadowsocks/install.sh
+++ install/shadowsocks/install.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 
+apt-get install python-pip -y
+pip install shadowsocks
+apt–get install python–m2crypto -y
 /usr/bin/python /usr/local/bin/ssserver -p 51218 -k Crackme666! -d start --log-file /root/shadowsocks.log -v
